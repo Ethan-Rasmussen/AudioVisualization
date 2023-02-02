ccea09992d8f4d9400f6063b43a5f72843aca398
morsbenj1 <113371432+morsbenj1@users.noreply.github.com>
2023-02-02 13:34:42 -0600

    Create SoundLogProcedure.py

---
+++ Scripts/SoundLogProcedure.py
@@ -0,0 +1,12 @@
+import RawSound
+import sensor_threading
+
+# Acquire 4 times using the sensor_threading module
+times = sensor_threading.acquire_times()
+
+# Create an object of type RawSound
+sound = RawSound.RawSound(times[0], times[1], times[2], times[3], 0.0, 0.0, 0.0)
+
+# Save the sound to a datafile
+filename = "sound_data.txt"
+sound.SaveSound(filename)
