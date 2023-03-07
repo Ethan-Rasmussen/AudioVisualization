1804aa021a224fe6821452e23b28bc634789811e
baileyr673 <114078090+baileyr673@users.noreply.github.com>
2023-03-07 15:38:28 -0600

    Add files via upload

--- Scripts/GUItest.py
+++ Scripts/GUItest.py
@@ -1,38 +1,92 @@
 from tkinter import * 
-#from matplotlib.figure import Figure
-#from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
-#NavigationToolbar2Tk)
-import matplotlib.pyplot as plt
+import time
 
-vert = [5, -4.2, 6.2, 2.1, 3.2]
-horiz = [-5, 2.2, 3.4, 6.1, 1]
+
+lines = 0
 
 window = Tk()
-C = Canvas(window, height = 400, width = 400, bg = 'white')
 
-def plot(): #create scatter plot - not very good for our purposes
-    plt.scatter(vert, horiz)
-    plt.title("Sound Locations")
-    plt.show()
+menubar = Menu(window)
+window.config(menu=menubar)
+
+
+def saveFile():
+    #connect save file function call HERE
+    clickText = Label(window, text = "File Saved", bg = 'green').place(x = 10,y = 10)
+def loadFile():
+    #connect load file function call HERE
+    clickText = Label(window, text = "File Loaded", bg = 'green').place(x = 10,y = 10)
+
+def initCalWindow():
+    #connect calibrate function calls HERE
+    cal_window = Toplevel(window)
+    cal_window['bg'] = 'light blue'
+    cal_window.geometry("300x300")
+    cal_window.title("Calibration Settings")
+
+    inputtxt1 = Text(cal_window,
+                       height = 1,
+                       width = 20,
+                       bg = 'pink'
+                       )
+    
+    mic1label = Label(cal_window, text = "Microphone 1", bg = 'white').place(x = 15,y = 10)
+    inputtxt1.place(x=100, y =10)
+
+    inputtxt2 = Text(cal_window,
+                       height = 1,
+                       width = 20,
+                       bg = 'white')
+    
+    mic2label = Label(cal_window, text = "Microphone 2", bg = 'white').place(x = 15,y = 40)
+    inputtxt2.place(x=100, y=40)
+
+    inputtxt3 = Text(cal_window,
+                   height = 1,
+                   width = 20,
+                   bg = 'pink'
+                   )
+    
+    mic3label = Label(cal_window, text = "Microphone 3", bg = 'white').place(x = 15,y = 70)
+    inputtxt3.place(x=100,y=70)
+
+    inputtxt4 = Text(cal_window,
+                       height = 1,
+                       width = 20,
+                       bg = 'white')
+    mic4label = Label(cal_window, text = "Microphone 4", bg = 'white').place(x = 15,y = 100)
+    inputtxt4.place(x=100, y=100)
+
 
 def point():
-      xval = inputtxtX.get(1.0, "end-1c") #get val from x box
-      yval = inputtxtY.get(1.0, "end-1c") #get val from y box
+    global lines
+    lines = lines + 1
 
-      #to hoop up to main program assign xval and yval to correct *ADJUSTED* values from regression results
+    if(lines >= 2):
+        lines = 1
+        C.delete('alive')
+    
+    xval = inputtxtX.get(1.0, "end-1c") #get val from x box //get predicted value from regression for A
+    yval = inputtxtY.get(1.0, "end-1c") #get val from y box //get predicted value from regression for B
 
-      point = C.create_oval(float(xval),float(yval),float(xval)+5.0,float(yval)+5.0, fill = 'red')
+    #convert A into pixels 40 pixels = 1 (foot?) so xval * 40 scales to # of pixels (currently max value of 5)
+    convAval = (float(xval) * 40.0) + 200 #A as a pixel number
+    convBval = (float(yval) * 40.0) + 200 #B as a pixel number
 
-#*INFORMATION FOR ADJUSTMENTS FORMULA*
-
-#400 by 400 canvas - (0,0 is top left corner)
-#actual origin is located at (200,200) so equation to convert to pixels needs to include an offset of +200x and +200y to start at the origin
-#positive values need to become negative to be in top half
-#negative values are positive to be in bottom half i.e 245 = -45 pixels, 160 = +40 pixels
-#every box is 40x40 (= 1 foot? 40/12 = 3.33 pixels per inch)
-#graph covers 5 feet in any direction
+    #Y = -Ax / B
+    # X = -By /A
+    Y = (-convAval * 0) / convBval
+    X = (-convBval * 0) / convAval
+    #to hook up to main program assign xval and yval to correct values from regression results
+    guess = C.create_line(200,200,convAval,convBval, fill = 'red', tags = 'alive')
+    window.after(1000, point)
 
 C = Canvas(window, height = 400, width = 400, bg = 'white')
+
+menubar.add_command(label = "Save", command = saveFile, activebackground='grey')
+menubar.add_command(label = "Load", command = loadFile, activebackground='grey')
+menubar.add_command(label = "Calibrate", command = initCalWindow, activebackground='grey')
+
 xaxis = C.create_line(200,0, 200,400, fill='black')
 yaxis = C.create_line(0,200, 400,200, fill='black')
 xline1 = C.create_line(160,0, 160,400, fill='grey')
@@ -98,7 +152,7 @@ point_button = Button(master = window,
                       command = point,
                       height = 2,
                       width = 10,
-                      text = "Plot Point")
+                      text = "Start Plotting")
 
 point_button.pack()
 
