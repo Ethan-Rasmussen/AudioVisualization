8d64bb46a182f1dca2975dc5d9edec42241ddab0
morsbenj1 <113371432+morsbenj1@users.noreply.github.com>
2023-04-17 19:53:41 -0500

    Add files via upload

---
+++ Scripts/Regression_SandBox/TestListener.py
@@ -0,0 +1,21 @@
+import sys
+import pathlib
+import time
+from RawSound import RawSound
+from RawSoundBuffer import RawSoundBuffer
+
+def print_sound_data(raw_sound):
+    print(f"Times: {raw_sound.times}")
+    print(f"A: {raw_sound.a}")
+    print(f"B: {raw_sound.b}")
+    print(f"C: {raw_sound.c}")
+
+if __name__ == '__main__':
+    next_sound = RawSound()
+    while True:
+        try:
+            next_sound = RawSoundBuffer.get_next_sound()
+            if next_sound != None:
+                print_sound_data(next_sound)
+        except KeyboardInterrupt:
+            break
