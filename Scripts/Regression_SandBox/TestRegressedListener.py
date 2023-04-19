2e94da1c3ca456111b0bccafa717aad50ede95e7
Ethan-Rasmussen <110521725+Ethan-Rasmussen@users.noreply.github.com>
2023-04-19 15:58:58 -0500

    Update TestRegressedListener.py
    Imports the conversion.py so that we have the ability to turn our parameters into coordinates

--- Scripts/Regression_SandBox/TestRegressedListener.py
+++ Scripts/Regression_SandBox/TestRegressedListener.py
@@ -4,6 +4,10 @@ import time
 from RawSound import RawSound
 from RegressedSoundBuffer import RegressedSoundBuffer
 from RawSoundBuffer import RawSoundBuffer
+from SoundConversion import SoundConversion
+#from LiveFeedModel import add_data_to_plot
+
+#converted_sound_coord = []
 
 def print_sound_data(regressed_sound):
     print(f"Times:")
@@ -18,16 +22,34 @@ def print_sound_data(regressed_sound):
 #     print(f"B (actual): {regressed_sound.actual_b}")
 #     print(f"C (actual): {regressed_sound.actual_c}")
 
-if __name__ == '__main__':
-    next_sound = RawSound()
-    regressed_sound_buffer = RegressedSoundBuffer()
-    while True:
-        try:
-            next_sound = RawSoundBuffer.get_next_sound()
-            if next_sound != None:
-                regressed_sound_buffer.add_raw_sound(next_sound)
-                regressed_sound = regressed_sound_buffer.get_regressed_sound()
-                if regressed_sound != None:
-                    print_sound_data(regressed_sound)
-        except KeyboardInterrupt:
-            break
+def print_coordinates(converted_sound):
+    print(f"X: {converted_sound.x}")
+    print(f"Y: {converted_sound.y}")
+    print(f"Z: {converted_sound.z}")
+
+
+#if __name__ == '__main__':
+
+next_sound = RawSound()
+regressed_sound_buffer = RegressedSoundBuffer()
+while True:
+    try:
+        next_sound = RawSoundBuffer.get_next_sound()
+        if next_sound != None:
+            regressed_sound_buffer.add_raw_sound(next_sound)
+            regressed_sound = regressed_sound_buffer.get_regressed_sound()
+            if regressed_sound != None:
+                print_sound_data(regressed_sound)
+                converted_sound = SoundConversion(a=regressed_sound.a, b=regressed_sound.b, c=regressed_sound.c)
+                regressed_sound.a = regressed_sound.a*100000
+                regressed_sound.b = regressed_sound.b*100000
+                regressed_sound.c = regressed_sound.c*100000
+                global converted_sound_coord
+                converted_sound_coord = [converted_sound.x, converted_sound.y, converted_sound.z]
+                #converted_sound_coord = [regressed_sound.a, regressed_sound.b, regressed_sound.c]
+                print_coordinates(converted_sound)
+                #add_data_to_plot([regressed_sound.a, regressed_sound.b, regressed_sound.c])
+                break
+    except KeyboardInterrupt:
+        break
+
